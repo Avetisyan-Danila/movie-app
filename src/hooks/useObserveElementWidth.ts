c651307485cd3887195ce9c00216547fe16ed751
Danila <avetisyandanila21@yandex.ru>
2024-04-13 12:47:32 +0300

    sidebar menu + search

--- src/hooks/useObserveElementWidth.ts
+++ src/hooks/useObserveElementWidth.ts
@@ -5,18 +5,24 @@ export const useObserveElementWidth = <T extends HTMLElement>() => {
   const refElement = useRef<T>(null);
 
   useEffect(() => {
+    // Сохраняем текущее значение ref в переменной внутри эффекта
+    const currentElement = refElement.current;
+
     const observer = new ResizeObserver((entries) => {
       setWidth(entries[0].target.getBoundingClientRect().width);
     });
 
-    if (refElement.current) {
-      observer.observe(refElement.current);
+    if (currentElement) {
+      observer.observe(currentElement);
     }
 
     return () => {
-      refElement.current && observer.unobserve(refElement.current);
+      // Используем сохраненное значение ref в функции очистки
+      if (currentElement) {
+        observer.unobserve(currentElement);
+      }
     };
-  }, []);
+  }, []); // Зависимости остаются пустыми, так как ref и функция setState не изменяются
 
   return {
     elementWidth,
