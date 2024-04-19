5c76b932751eb367aac37fded861a3a9c0174fc8
Danila <avetisyandanila21@yandex.ru>
2024-04-19 23:53:11 +0300

    Heading

--- src/components/Heading/Heading.props.ts
+++ src/components/Heading/Heading.props.ts
@@ -2,5 +2,8 @@ import { ReactNode } from 'react';
 
 export interface HeadingProps {
   children: ReactNode;
+
+  className?: string;
+  appearance?: 'big' | 'small';
   withMargin?: boolean;
 }
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,11 +2,18 @@ import { HeadingProps } from './Heading.props.ts';
 import styles from './Heading.module.css';
 import cn from 'classnames';
 
-export const Heading = ({ children, withMargin = true }: HeadingProps) => {
+export const Heading = ({
+  children,
+  className,
+  withMargin = true,
+  appearance = 'big',
+}: HeadingProps) => {
   return (
     <h3
-      className={cn(styles['heading'], {
+      className={cn(styles['heading'], className, {
         [styles['with-margin']]: withMargin,
+        [styles['big']]: appearance === 'big',
+        [styles['small']]: appearance === 'small',
       })}
     >
       {children}
