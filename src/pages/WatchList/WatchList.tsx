38eab3a1bac6e768f38ab55169b917e2e40a9b91
Danila <avetisyandanila21@yandex.ru>
2024-04-21 14:17:04 +0300

    Сделал страницу "Смотреть позже"

--- src/pages/WatchList/WatchList.tsx
+++ src/pages/WatchList/WatchList.tsx
@@ -1,9 +1,20 @@
 import { Heading } from '../../components/Heading/Heading.tsx';
+import { FilmListWithPagination } from '../../components/FilmListWithPagination/FilmListWithPagination.tsx';
 
 export const WatchList = () => {
+  const collectionName = 'watchList';
+  const orderBy = 'createdAt';
+  const emptyMessage = 'Список "Смотреть позже" пуст';
+
   return (
     <>
       <Heading>Смотреть позже</Heading>
+
+      <FilmListWithPagination
+        collectionName={collectionName}
+        orderBy={orderBy}
+        emptyMessage={emptyMessage}
+      />
     </>
   );
 };
