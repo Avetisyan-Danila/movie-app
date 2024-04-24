db850ec5437ffe06859423eaa1a05c20cffd50d5
Danila <avetisyandanila21@yandex.ru>
2024-04-24 22:23:16 +0300

    Добавил список ошибок при работе с авторизацией

---
+++ src/helpers/AuthErrorMap.ts
@@ -0,0 +1,20 @@
+export default {
+  'auth/user-not-found': 'Пользователь с таким Email не найден.',
+  'auth/wrong-password': 'Неверный пароль.',
+  'auth/weak-password': 'Пароль должен состоять из 6 символов.',
+  'auth/too-many-requests':
+    'Из-за многочисленных неудачных попыток запрос был отклонен. Повторите попытку позже.',
+  'auth/email-already-in-use': 'Этот email уже используется.',
+
+  'login-unknown-error': 'Произошла ошибка при авторизации. Попробуйте позже.',
+  'register-unknown-error':
+    'Произошла ошибка при регистрации. Попробуйте позже.',
+  'logout-unknown-error':
+    'Произошла ошибка при попытке выхода из аккаунта. Попробуйте позже.',
+  'updateEmail-unknown-error':
+    'Произошла ошибка при смене Email. Попробуйте позже.',
+  'updatePassword-unknown-error':
+    'Произошла ошибка при смене пароля. Попробуйте позже.',
+  'deleteAccount-unknown-error':
+    'Произошла ошибка при удалении аккаунта. Попробуйте позже.',
+} as Record<string, string>;
